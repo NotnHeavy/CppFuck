e1aa08e97ce9cf60e29b7188f43dc09f401be4ea
NotnHeavy <94223272+NotnHeavy@users.noreply.github.com>
2021-11-13 13:03:25 +0000

    Add project files.

---
+++ C++Fuck/src/Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <version.h>
+
+#include <stdio.h>
+
+static void run(const char* const code)
+{
+	std::cout << "To be worked on!" << std::endl;;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc > 1) // Load file.
+	{
+		FILE* file;
+		fopen_s(&file, argv[1], "r");
+		if (file == 0)
+		{
+			std::cout << "The file \"" << argv[1] << "\" does not exist on your system." << std::endl;
+			return 1;
+		}
+		fseek(file, 0, SEEK_END);
+		const static volatile unsigned long long int size = ftell(file); // Very hilarious.
+		char* contents = new char[size];
+		fseek(file, 0, SEEK_SET);
+		fread(contents, size, 1, file);
+		fclose(file);
+		run(contents);
+		delete[] contents;
+	}
+	else // Interactive mode.
+	{
+		std::cout << "C++Fuck " << FVERSION_STRINGIZED << " main (compiled on " << __DATE__ << ", " << __TIME__ << " with MSVC, for win64)" << std::endl;
+		std::string input;
+		while (true)
+		{
+			std::cout << ">>> ";
+			std::getline(std::cin, input);
+			run(input.c_str());
+		}
+	}
+}
---
+++ C++Fuck/version.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <FileVersion.h>
+#define return_to_string(s) #s // Stringize function.
+#define to_string(s) return_to_string(s)
+
+#define DESCRIPTION "A feature-rich Brainfuck virtual machine."
+#define FVERSION V_MAJOR, V_MINOR, V_BUILD, V_REVISION // Same as the product version.
+#define FVERSION_STRINGIZED to_string(V_MAJOR) "." to_string(V_MINOR) "." to_string(V_BUILD) "." to_string(V_REVISION) // Same as the stringized product version.
+#define PRODUCTNAME "C++Fuck"
+#define ORIGINALNAME PRODUCTNAME ".exe" // Same as internal name.
+#define COPYRIGHT "NotnHeavy, since 2021"
+
+#ifdef _DEBUG
+#define IS_DEBUG VS_FF_DEBUG
+#else
+#define IS_DEBUG 0
+#endif
+
+#define OS VOS_NT_WINDOWS32
+#define FTYPE VFT_APP // For DLLs, do VFT_DLL.
